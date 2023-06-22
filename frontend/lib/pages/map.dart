294329e4cc86ccfb21fa91a03c6ae38ae2f396a2
Aleksei Sokol <kanootoko@gmail.com>
2023-06-22 18:26:06 +0300

    Multiple compositions display
    Changes:
    - add composition choice buttons after method launch
    - bump backend version
    - bump frontend version
    - update docker-compose.yml to latest versions

--- frontend/lib/pages/map.dart
+++ frontend/lib/pages/map.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:landscaping_frontend/models/plants.dart';
 import 'package:landscaping_frontend/notifiers/compositions.dart';
-import 'package:landscaping_frontend/pages/plants_list.dart';
 import 'package:landscaping_frontend/widgets/choose_options.dart';
 import 'package:landscaping_frontend/widgets/landscape_map.dart';
+import 'package:landscaping_frontend/widgets/results_table.dart';
 import 'package:provider/provider.dart';
-
-import '../models/plants.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
@@ -35,56 +34,6 @@ class MapPage extends StatelessWidget {
               child: ResultsTable(
                   compositions: compositions, close: closeCallback),
             )
-        ],
-      ),
-    );
-  }
-}
-
-class ResultsTable extends StatelessWidget {
-  const ResultsTable(
-      {super.key, required this.compositions, required this.close});
-
-  final List<Plants> compositions;
-  final void Function() close;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey.shade400.withAlpha(210),
-      child: Column(
-        children: [
-          Container(
-            color: const Color.fromARGB(255, 230, 199, 107),
-            child: Center(
-              child: SizedBox(
-                child: Container(
-                  height: 40,
-                  width: 1172,
-                  padding: const EdgeInsets.all(2.0),
-                  alignment: Alignment.centerRight,
-                  child: ElevatedButton(
-                      onPressed: close,
-                      child: Icon(
-                        Icons.cancel_presentation_outlined,
-                        color: Colors.red.shade400,
-                      )),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.green.shade200,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: PlantsTable(
-                  compositions[0].plants,
-                  withSwitch: false,
-                ),
-              ),
-            ),
-          ),
         ],
       ),
     );
